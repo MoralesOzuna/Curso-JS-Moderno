9bb2d12fbde3ea1a6ad00892a401177c0d13cd49
Jose Morales <moraleseduardoo541@gmail.com>
2024-10-01 21:12:58 -0700

    01-Introudccion

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,8 @@
+const nombre = prompt('¿Cúal es tu nombre');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavasCript Moderno`
+
+//Asigna el valor hacia la variable producto
+
+const producto = 'Monitor 24 pulgadas';
