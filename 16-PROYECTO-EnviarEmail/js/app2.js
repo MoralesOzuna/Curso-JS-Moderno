686948aa20f8758fff9ed1d54a644fb8bcf2a845
Jose Morales <moraleseduardoo541@gmail.com>
2025-01-07 21:22:40 -0800

    Enviar Email v2 finished

--- 16-PROYECTO-EnviarEmail/js/app2.js
+++ 16-PROYECTO-EnviarEmail/js/app2.js
@@ -1,21 +1,72 @@
 //Se ejecuta despues de descargar el contenido HTML O DOM 
 document.addEventListener('DOMContentLoaded', function() {
 
+
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     //Seleccionar los elementos de la interaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"');
+    const btnReset = document.querySelector('#formulario button[type="reset"');
+    const spinner = document.querySelector('#spinner');
 
     //Asignar eventos
 
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
     inputMensaje.addEventListener('blur', validar);
+
+    formulario.addEventListener('submit', enviarEmail);
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault(); //Previene el comportamiento por default que es resetear los datos
+
+
+        //Reiniciamos el objeto
+        resetFormulario();
+    });
+
+    function enviarEmail(e){
+
+        e.preventDefault(); //Previene el comportamiento por default que es enviar los datos
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            
+            //Reiniciar el codigo
+
+            resetFormulario();
+
+            //Crear una alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+            formulario.appendChild(alertaExito);
+
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+           
+        }, 3000);
+
+        
+
+    }
     
     
     function validar(e){
-        console.log(e.target);
         if(e.target.value.trim() === ''){ //.trim() elimina los espacios vacios
 
             /* .parentElement te permite ir hacia el padre del elemento
@@ -23,13 +74,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
             Ejemplo: e.target.parentElement.nextElementSibling
             con estos parametros puedes recorrer tu html
-
-                     
             */
             mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        } else{
-            console.log('Sí hay algo');
+            email[e.target.name] = ''; //Reiniciamos datos del objeto
+            comprobarEmail(); //desabhilitamos el boton
+            return; //Si se activa el if, el return corta la ejecucion del codigo
+        } 
+
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El Email no es valido', e.target.parentElement);
+            email[e.target.name] = ''; //Reiniciamos datos del objeto
+            comprobarEmail();
+            return;
         }
+
+        limpiarAlerta(e.target.parentElement);
+
+        //Asignamos valores al objeto
+
+
+        //email[email, asunto, mensaje] = el valor escrito en email, asunto, mensaje, sin espacios y en minusculas
+        //email[email, asunto, mensaje] nos va permitiendo acceder a las propiedades en el objeto de forma dinamica
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+
+        //Comprobar el objeto de email 
+        comprobarEmail()
+
+
+        
 
 
     }
@@ -37,15 +111,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function mostrarAlerta(mensaje, referencia){
 
-        //Comprueba si ya existe una alerta
-
-        const alerta = referencia.querySelector('.bg-red-600');
-
-
-        if(alerta){
-            alerta.remove();
-        }
-
+       limpiarAlerta(referencia);
+        
         //Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
@@ -55,6 +122,59 @@ document.addEventListener('DOMContentLoaded', function() {
         //Inyectar el error en el formulario
         referencia.appendChild(error);
 
+    }
+
+
+    function limpiarAlerta(referencia){
+        //Comprueba si ya existe una alerta
+
+        /* Si coloco document.querySelector, se limpian todas las alertas en el documento
+        utilizando la referencia buscamos desde un punto especifico */
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+
+    function validarEmail(email){
+
+        //Expresion regular
+        //Es una serie de valores que conforman un patron
+        const regex  =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+
+    }
+
+
+    function comprobarEmail(){
+        //Esto retorna un arreglo
+        //console.log(Object.values(email)); Retorna los valores Object.keys(objeto) retorna las llaves
+        //includes busca un string vacio
+        //includes es una propiedad de los arrays
+
+        if(Object.values(email).includes('')){
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return
+        } 
+
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+
+
+    }
+
+    function resetFormulario(){
+          //Reiniciamos el objeto
+          email.email = '';
+          email.asunto = '';
+          email.mensaje = '';
+
+
+          formulario.reset();
+          comprobarEmail();
     }
 
 
