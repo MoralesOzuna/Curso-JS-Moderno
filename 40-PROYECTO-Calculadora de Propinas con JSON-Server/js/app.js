5c334bb845db75742cb93985aba86bac7b1802a9
Jose Morales <moraleseduardoo541@gmail.com>
2025-12-26 23:18:49 -0800

    Agregamos contenido  a la propiedad pedido del objeto cliente

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -1,8 +1,14 @@
 let cliente = {
     mesa: '',
     hora: '',
-    pedido: ''
+    pedido: []
 };
+
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
 
 
 
@@ -52,6 +58,9 @@ function guardarCliente(){
     //MOstrar las secciones
     mostrarSecciones();
 
+    //obtener platillos de la API de JSON-server
+    obtenerPlatillos();
+
 
 }
 
@@ -59,4 +68,82 @@ function guardarCliente(){
 function mostrarSecciones(){
     const seccionesOcultas = document.querySelectorAll('.d-none');
     seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos(){
+    const url = 'http://localhost:3000/platillos';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarPlatillos(resultado))
+        .catch(error => console.log(error)) //por si falla se activa .catch
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[platillo.categoria]; //objeto[propiedad] es lo mismo que decir objeto.llave pero más dinamico.
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+
+
+        //Funcion que detecta la cantidad y el platillo que se esta agregando
+
+        /* agregarPlatillo(platillo.id) esto no puede hacerse porque ativa la  funcion agregar platillo */
+        inputCantidad.onchange = function(){ //onchange es un addevenetListener para elementos al vuelo;
+            const cantidad = parseInt(inputCantidad.value);
+            agregarPlatillo({...platillo, cantidad}) //Pasamos el objeto platillo y la cantidad
+        }; 
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+
+        
+
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
+
+        contenido.appendChild(row);
+    })
+}
+
+function agregarPlatillo(producto){ //producto es el ultimo platillo que agrego
+    //Extraer el pedido actual
+
+    let {pedido} = cliente;
+
+    //Revisar que la cantidad sea mayor a 0
+
+    if(producto.cantidad > 0){
+        cliente.pedido = [...pedido, producto];
+    } else{
+        console.log('No es mayor a 0')
+    }
+
+    console.log(cliente.pedido)
 }
